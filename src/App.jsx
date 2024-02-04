b6ff678ee5b0f43e113db1ab5d897a698d9c7a1a
devR2001 <144663791+devR2001@users.noreply.github.com>
2024-02-04 13:32:02 +0100

    First Steps Towards Our Tic-Tac-Toe Game

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 function App() {
   return (
-    // <header>
-    //   <img src="" alt="" />
-    //   <h1>Tic-Tac-Toe</h1>
-    // </header>
-    <h1>Coming Soon</h1>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </main>
   );
 }
 
